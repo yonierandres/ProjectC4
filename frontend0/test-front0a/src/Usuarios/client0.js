fa9661b7e733c717a7039b64173de47fa07492e7
inghoro <ing.horo@gmail.com>
2021-12-17 23:49:24 -0500

    actualizacion token personal, se esta borrando

--- frontend0/test-front0a/src/Usuarios/client0.js
+++ frontend0/test-front0a/src/Usuarios/client0.js
@@ -9,7 +9,9 @@ const client0 = new ApolloClient({
     // header: "content-type: application/json",
     headers: 
             {
-                Authorization: "Bearer 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"
+                // Authorization: "Bearer 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"
+                Authorization: "Bearer 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 "
+                
             },            
 });
 
